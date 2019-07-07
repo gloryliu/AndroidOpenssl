944c8c28af81709e8891e521059d7623b06484cb
liuzhenrong <liuzhenrong@huobi.com>
2019-07-07 19:55:36 +0800

    openssl

---
+++ MyApplication2/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,207 @@
+#include <jni.h>
+#include <string>
+#include <stdio.h>
+#include <android/log.h>
+#include "openssl/md5.h"
+#include "openssl/evp.h"
+#include "openssl/aes.h"
+#include "openssl/err.h"
+
+void handleErrors(void);
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext);
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *plaintext);
+int test();
+
+extern "C" JNIEXPORT jstring JNICALL
+
+Java_com_liu_myapplication_MainActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    test();
+    return env->NewStringUTF(hello.c_str());
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_liu_myapplication_MainActivity_stringFromMD5(
+        JNIEnv *env,
+        jobject thiz/* this */,
+        jstring srcjStr) {
+
+    const char *unicodeChar = env->GetStringUTFChars(srcjStr, NULL);
+    size_t unicodeCharLength = env->GetStringLength(srcjStr);
+
+    unsigned char md[MD5_DIGEST_LENGTH];
+    int i;
+    char buf[33] = {'\0'};
+    MD5((unsigned char*)unicodeChar, unicodeCharLength, (unsigned char*)&md);
+    for (i = 0; i < 16; i++) {
+        sprintf(&buf[i*2], "%02x", md[i]);
+    }
+    env->ReleaseStringUTFChars(srcjStr, unicodeChar);
+
+
+    //char Key[] = {8, -125, -44, -1, 4, 16, -108, -93, -121, -23, 5};
+    //char source[] = {99, 124, 110, -36, -106, 32, 99, -69, -38, -71, 109, 60, -65, -61, -79, 29, -126, 85, -107, 122, -6, 9, 77, 17, -95, 54, -65, -106, 38, -20, 95, -109};
+
+
+    //char IMKey[] = {16, 27, -6, -79, -121, 44, -57, -29, 113, -33, 122, -59, -17, -105, 66, 120};
+
+    return env->NewStringUTF(buf);
+}
+
+int test()
+{
+    /*
+     * Set up the key and iv. Do I need to say to not hard code these in a
+     * real application? :-)
+     */
+
+    /* A 256 bit key */
+    unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
+
+    /* A 128 bit IV */
+    unsigned char *iv = (unsigned char *)"0123456789012345";
+
+    /* Message to be encrypted */
+    unsigned char *plaintext =
+            (unsigned char *)"The quick brown fox jumps over the lazy dog";
+
+    /*
+     * Buffer for ciphertext. Ensure the buffer is long enough for the
+     * ciphertext which may be longer than the plaintext, depending on the
+     * algorithm and mode.
+     */
+    unsigned char ciphertext[128];
+
+    /* Buffer for the decrypted text */
+    unsigned char decryptedtext[128];
+
+    int decryptedtext_len, ciphertext_len;
+
+    /* Encrypt the plaintext */
+    ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
+                              ciphertext);
+
+    /* Do something useful with the ciphertext here */
+    printf("Ciphertext is:\n");
+    BIO_dump_fp (stdout, (const char *)ciphertext, ciphertext_len);
+
+    /* Decrypt the ciphertext */
+    decryptedtext_len = decrypt(ciphertext, ciphertext_len, key, iv,
+                                decryptedtext);
+
+    /* Add a NULL terminator. We are expecting printable text */
+    decryptedtext[decryptedtext_len] = '\0';
+
+    /* Show the decrypted text */
+    printf("Decrypted text is:\n");
+    printf("%s\n", decryptedtext);
+
+
+}
+
+void handleErrors(void)
+{
+    unsigned long errCode;
+
+    printf("An error occurred\n");
+    while(errCode = ERR_get_error())
+    {
+        char *err = ERR_error_string(errCode, NULL);
+        printf("%s\n", err);
+    }
+    abort();
+}
+
+int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *ciphertext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int ciphertext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
+
+    /*
+     * Initialise the encryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /*
+     * Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+        handleErrors();
+    ciphertext_len = len;
+
+    /*
+     * Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len))
+        handleErrors();
+    ciphertext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return ciphertext_len;
+}
+
+int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+            unsigned char *iv, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+
+    int len;
+
+    int plaintext_len;
+
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new()))
+        handleErrors();
+
+    /*
+     * Initialise the decryption operation. IMPORTANT - ensure you use a key
+     * and IV size appropriate for your cipher
+     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
+     * IV size for *most* modes is the same as the block size. For AES this
+     * is 128 bits
+     */
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
+        handleErrors();
+
+    /*
+     * Provide the message to be decrypted, and obtain the plaintext output.
+     * EVP_DecryptUpdate can be called multiple times if necessary.
+     */
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
+        handleErrors();
+    plaintext_len = len;
+
+    /*
+     * Finalise the decryption. Further plaintext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
+        handleErrors();
+    plaintext_len += len;
+
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
+
+    return plaintext_len;
+}
